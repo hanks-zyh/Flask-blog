37811651b78b8f97529312813a2eda22cb31c61c
hanks <zhangyuhan@nashangban.com>
2015-08-11 23:26:13 +0800

    add bottom pagination

--- app/main/views.py
+++ app/main/views.py
@@ -6,7 +6,7 @@ from flask.ext.login import current_user
 __author__ = 'Hanks'
 
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app, request
 
 from . import main
 from .forms import NameForm, PostForm
@@ -22,8 +22,13 @@ def index():
         post = Post(body=form.body.data, author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('main.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+    # posts = Post.query.order_by(Post.timestamp.desc()).all()
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts, pagination=pagination)
 
 
 @main.route('/user/<username>')
--- config.py
+++ config.py
@@ -28,6 +28,7 @@ class DevelopmentConfig(Config):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SENDER = '1143123897@qq.com'
+    FLASKY_POSTS_PER_PAGE = 20
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
