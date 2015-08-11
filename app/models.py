7037dd192f2da7b5381a053a8cc94d76b56c4b9c
hanks <zhangyuhan@nashangban.com>
2015-08-12 00:46:31 +0800

    body_html when change body

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@
 # -*- coding:utf-8 -*-
 from datetime import datetime
 import hashlib
+import bleach
+from markdown import markdown
 
 __author__ = 'Hanks'
 
@@ -205,6 +207,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -222,3 +225,16 @@ class Post(db.Model):
                      author=u)
             db.session.add(p)
             db.session.commit()
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(
+            bleach.clean(
+                markdown(value, output_format='html'),
+                tags=allowed_tags, strip=True))
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
